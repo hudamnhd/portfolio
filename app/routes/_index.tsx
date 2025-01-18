196fb5f378e0edb155bcdfd8e230100d510cf2f6
hudamnhd <muhamadhuda519@gmail.com>
2025-01-19 00:15:37 +0700

    - delete lucide

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,52 @@ import { Button, buttonVariants } from "#app/components/ui/button";
 import { Link } from "react-router";
 import { cn } from "#app/utils/misc";
 import { Theme, useTheme } from "remix-themes";
-import { Sun, Moon } from "lucide-react";
 
 function ThemeSwitch() {
 	const [theme, setTheme] = useTheme();
 
 	const nextMode = theme === "light" ? "dark" : "light";
 	const modeLabel = {
-		light: <Sun />,
-		dark: <Moon />,
+		light: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				className="lucide lucide-sun"
+			>
+				<circle cx="12" cy="12" r="4" />
+				<path d="M12 2v2" />
+				<path d="M12 20v2" />
+				<path d="m4.93 4.93 1.41 1.41" />
+				<path d="m17.66 17.66 1.41 1.41" />
+				<path d="M2 12h2" />
+				<path d="M20 12h2" />
+				<path d="m6.34 17.66-1.41 1.41" />
+				<path d="m19.07 4.93-1.41 1.41" />
+			</svg>
+		),
+		dark: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				className="lucide lucide-moon"
+			>
+				<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+			</svg>
+		),
 	};
 
 	return (
